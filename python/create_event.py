5eb24cfb268d8ef553143f8adba83e34bbdc3563
unknown <DK@.(none)>
2014-06-28 19:53:21 -0400

    .

--- python/create_event.py
+++ python/create_event.py
@@ -3,11 +3,13 @@ import MySQLdb
 import cgi
 
 print "Content-type: text/html\n\n"
-data = cgi.FieldStorage();
-conn = MySQLdb.connect (host = "207.210.106.21",user = "cpboat",passwd = "ZH7bjgPZ",db = "cpboat")
-cursor = conn.cursor ()
-cursor.execute("insert into events(date) values(%s)", [data['date'].value])
-conn.commit()
-cursor.close()
-conn.close()
-print 'success'
+print common.ADMINS
+if common.check_auth():
+    data = cgi.FieldStorage();
+    conn = MySQLdb.connect (host = "207.210.106.21",user = "cpboat",passwd = "ZH7bjgPZ",db = "cpboat")
+    cursor = conn.cursor ()
+    cursor.execute("insert into events(date) values(%s)", [data['date'].value])
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print 'success'
