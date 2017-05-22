7f032af9547c4c1e77ac326ac77a2d3d1217ff69
DK <danielkim@soulkast.com>
2017-05-22 19:41:45 -0400

    cleaned up debug output and some indentations

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var MOBILE = false;
 function statusChangeCallback(response) {
 	
 	if (response.status === 'connected') {
-  		// Logged into your app and Facebook.
+  	// Logged into your app and Facebook.
 		ACCESS_TOKEN =   FB.getAuthResponse()['accessToken'];
-     	loadInfo();
+    loadInfo();
 
 	} else if (response.status === 'not_authorized') {
 		// The person is logged into Facebook, but not your app.
@@ -32,15 +32,15 @@ var logout_event = function(response) {
 
 window.fbAsyncInit = function() {
 	FB.init({
-    	appId      : '1444881325765700',
+    appId      : '1444881325765700',
 		cookie     : true,  // enable cookies to allow the server to access 
 		xfbml      : true,  // parse social plugins on this page
 		version    : 'v2.0' // use version 2.0
-  	});
+  });
 
-  	FB.getLoginStatus(function(response) {
-    	statusChangeCallback(response);
-  	});
+	FB.getLoginStatus(function(response) {
+  	statusChangeCallback(response);
+	});
 	
 	FB.Event.subscribe('auth.logout', logout_event);
 };
@@ -57,8 +57,10 @@ window.fbAsyncInit = function() {
 function loadInfo() {
 	FB.api('/me',  function(response) {
 		MY_ID = response.id;
-		console.info(MY_ID);
-  		document.getElementById('status').innerHTML =
+  	if(window.location.href.startsWith('http://dev.')){
+  		console.info('User\'s ID:' + MY_ID);
+  	}
+  	document.getElementById('status').innerHTML =
 		'Hello, ' + response.name + '!';
       	
 		//check if t-pain is in town
@@ -140,50 +142,47 @@ function renderCal(){
 	    	if(MY_ID == null){
 	    		alert("You must log in to join or view the roster!");
 	    	}
-			else{
-		    	$("#roster").html("");
-		        $("#dialog" ).dialog({title : event.start.format()});
-		        $("#dialog").block({ message: null }); 
-				$.ajax({
-					type: "POST",
-			  		url: "./python/roster.py",
-			  		data: { action:"check", date: event.start.format(), access_token: ACCESS_TOKEN}
-				})
-			  	.done(function( msg ) {
-			    	if(parseInt(msg) > 0){
-			    		$("#withdraw").show();
-			    		$("#join").hide();
-			    		
-			    	}
-			    	else{
-			    		$("#withdraw").hide();
-			    		$("#join").show();
-			    	}
+				else{
+			    	$("#roster").html("");
+			        $("#dialog" ).dialog({title : event.start.format()});
+			        $("#dialog").block({ message: null }); 
 					$.ajax({
 						type: "POST",
 				  		url: "./python/roster.py",
-				  		data: { date: event.start.format(), action:"get"},
-				  		dataType:"json"
-					})
-				  	.done(function( json ) {
-						if(json.length > 0){
-							$("#compete").show();
-							$("#no_compete").hide();
-						}
-						else{
-							$("#compete").hide();
-							$("#no_compete").show();						
-						}
+				  		data: { action:"check", date: event.start.format(), access_token: ACCESS_TOKEN}
+					}).done(function( msg ) {
+				    	if(parseInt(msg) > 0){
+				    		$("#withdraw").show();
+				    		$("#join").hide();
+				    		
+				    	}
+				    	else{
+				    		$("#withdraw").hide();
+				    		$("#join").show();
+				    	}
+						$.ajax({
+							type: "POST",
+					  		url: "./python/roster.py",
+					  		data: { date: event.start.format(), action:"get"},
+					  		dataType:"json"
+						}).done(function( json ) {
+							if(json.length > 0){
+								$("#compete").show();
+								$("#no_compete").hide();
+							}
+							else{
+								$("#compete").hide();
+								$("#no_compete").show();						
+							}
 				    	for(var i = 0; i < json.length; i++){
-				    		//console.info(json[i].user);
 							FB.api(json[i].user,  function(response) {
 								$("#roster").append("<div style='margin-bottom:5px;'><a target='_blank' href='" + response.link + "'>" + response.name+ "</a></div>");
 						    });	
 				    	}
 				    	$("#dialog").unblock();
-				  	});			  		    	
-		  		});	  
-		  	}  
+					  });			  		    	
+			  	});	  
+			  }  
 	    },
 	    dayClick: function(date, allDay, jsEvent, view) {            
 	    	if(T_PAIN){
--- python/t_pain.py
+++ python/t_pain.py
@@ -4,13 +4,13 @@ import common
 import cgi
 
 print "Content-type: text/html\n\n";
-
 data = cgi.FieldStorage();
 access_token = data['access_token'].value;
 print_ = "{\"t_pain\":\"";
 print_ += "1" if common.am_i_t_pain(access_token) else "0";
 print_ += "\",\"friends\":\"";
 print_ += "1" if common.am_i_friends_w_t_pain(access_token) else "0";
+#print_ += "\",\"access_token\":\"" + access_token;
 print_ += "\"}";
 
 print print_
